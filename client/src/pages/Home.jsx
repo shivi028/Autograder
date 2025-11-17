77e6736d69e27d1e698432c0433929e7fa8255d6
shivi028 <tiwarishivi028@gmail.com>
2025-11-17 11:24:48 +0530

    Deployment Fixes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Footer } from "@/components/Footer";
-import InteractiveRoadmap from "./InteractiveRoadMap";
+import InteractiveRoadmap from "./InteractiveRoadmap";
 import AutograderHero from "./HeroSection";
 import FeaturesSection from "./FeaturesSection";
 import ComparisonSection from "./ComparisonSection";
